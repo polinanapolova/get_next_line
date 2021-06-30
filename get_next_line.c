85d8d34ec2686d635ca5b2086e76db361fe57cb5
Polina <napolitano.polina@gmail.com>
2021-06-30 14:34:14 +0300

    very finish

--- get_next_line.c
+++ get_next_line.c
@@ -1,98 +1,94 @@
 #include "get_next_line.h"
 
-int ft_free_and_return(char **keep, int ret)
+static int	ft_strnl(char *aux)
 {
-	ft_free(keep);
-	return (ret);
+	int	i;
+
+	i = 0;
+	while (*(aux + i) && *(aux + i) != '\n')
+		i++;
+	if ((int)ft_strlen(aux) == i)
+		return (-1);
+	return (i);
 }
 
-int		ft_find_newline(const char *s, int c)
+static int	ft_free(char **s)
 {
-	int		i;
-	char	*ret;
-
-	i = -1;
-	ret = (char *)s;
-	while (ret[++i])
-		if (ret[i] == (char)c)
-			return (i);
+	free(*s);
+	*s = NULL;
 	return (-1);
 }
 
-int		ft_fill_line(char **keep, char **line, int fd)
+static ssize_t	ft_reader(int fd, char **content, ssize_t *len_read)
 {
-	char	*tmp;
-	int		idx;
+	char	*buf;
+	char	*aux;
 
-	idx = ft_find_newline(keep[fd], '\n');
-	if (idx != -1 && keep[fd][idx] == '\n')
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (-1);
+	while (ft_strnl(*content) < 0 && *len_read > 0)
 	{
-		if (!(*line = ft_substr(keep[fd], 0, idx)))
-			return (ft_free_and_return(keep, -1));
-		if (!(tmp = ft_substr(keep[fd], idx + 1, ft_strlen(keep[fd]))))
-			return (ft_free_and_return(keep, -1));
-		ft_free(&keep[fd]);
-		keep[fd] = tmp;
+		*len_read = read(fd, buf, BUFFER_SIZE);
+		if (*len_read == -1)
+			break ;
+		*(buf + *len_read) = '\0';
+		aux = ft_strdup(*content);
+		free(*content);
+		if (!aux)
+			return (ft_free(&buf));
+		*content = ft_strjoin(aux, buf);
+		free(aux);
+		if (!*content)
+			return (ft_free(&buf));
 	}
-	else
-	{
-		if (!(*line = ft_strdup(keep[fd])))
-			return (ft_free_and_return(keep, -1));
-		ft_free(&keep[fd]);
-		return (0);
-	}
+	free(buf);
+	return (*len_read);
+}
+
+int	ft_line_generator(char *content, char **line, char **leftover)
+{
+	int		index;
+	ssize_t	len;
+
+	index = ft_strnl(content);
+	len = ft_strlen(content);
+	if (index == -1)
+		index = ft_strlen(content);
+	*line = ft_substr(content, 0, index);
+	if (!*line)
+		return (ft_free(&content));
+	if (index < (int)ft_strlen(content) - 1)
+		*leftover = ft_substr(content, index + 1, len);
+	free(content);
+	if (!*leftover)
+		return (-1);
 	return (1);
 }
 
-char	*ft_update_static(char **keep, char *buf, int fd)
+int	get_next_line(int fd, char **line)
 {
-	char *tmp;
+	static char	*leftover;
+	char		*content;
+	ssize_t		len_read;
 
-	if (!keep[fd])
-	{
-		if (!(keep[fd] = ft_strdup("")))
-		{
-			if (buf)
-				free(buf);
-			return (NULL);
-		}
-	}
-	if (!(tmp = ft_strjoin(keep[fd], buf)))
-	{
-		ft_free(&keep[fd]);
-		free(buf);
-		return (NULL);
-	}
-	ft_free(&keep[fd]);
-	keep[fd] = tmp;
-	return (keep[fd]);
-}
-
-int		get_next_line(int fd, char **line)
-{
-	int			input;
-	char		*buf;
-	static char	*keep[FOPEN_MAX];
-
-	if (BUFFER_SIZE <= 0 || fd < 0 || fd > FOPEN_MAX || !line
-			|| !(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
-		return (ft_free_and_return(keep, -1));
-	while ((input = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[input] = '\0';
-		if (!(keep[fd] = ft_update_static(keep, buf, fd)))
-			return (-1);
-		if (ft_find_newline(keep[fd], '\n') > -1)
-			break ;
-	}
-	free(buf);
-	if (input < 0)
+	if (!line || fd < 0 || fd > 4096 || BUFFER_SIZE <= 0)
 		return (-1);
-	else if (input == 0 && !keep[fd])
+	if (leftover)
 	{
-		if (!(*line = ft_strdup("")))
-			return (-1);
-		return (0);
+		content = ft_strdup(leftover);
+		free(leftover);
+		leftover = NULL;
 	}
-	return (ft_fill_line(keep, line, fd));
+	else
+		content = ft_strdup("");
+	if (!content)
+		return (-1);
+	len_read = 1;
+	if (ft_reader(fd, &content, &len_read) == -1)
+	{
+		free(content);
+		return (-1);
+	}
+	return (ft_line_generator(content, line, &leftover) && len_read > 0);
 }
